57205f8f4ca9714af09d01d3a9ccae38f3fa6d7b
Administrator <1220302613@qq.com>
2022-10-20 00:51:57 +0800

    update

--- hello_world.py
+++ hello_world.py
@@ -4,3 +4,5 @@ print(torch.cuda.is_available())
 print(torch.cuda.device_count() )# gpu数量
 print(torch.cuda.current_device())# 当前设备索引, 从0开始
 print(torch.cuda.get_device_name(0))# 返回gpu名字
+
+### 第一次尝试
